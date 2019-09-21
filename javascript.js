24cf7de942136bb51f9200fc6878d95816369242
Emma Weber <emma.weber327@gmail.com>
2019-09-20 21:03:54 -0600

    completed the JS build to send the html input information to the database and then put the information on the html based on what is in the database. Working on CSS.

--- javascript.js
+++ javascript.js
@@ -14,3 +14,45 @@ var firebaseConfig = {
   var destination = "";
   var firstTrain = "";
   var frequency = "";
+
+  $("#add").on("click", function(){
+      event.preventDefault();
+
+      firstTrain = $("#time").val()
+      frequency = $("#minutes").val()
+      var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years")
+      var currentTime = moment();
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var tRemainder = diffTime % frequency;
+      var tMinutesTillTrain = frequency - tRemainder;
+      var nextTrainEq = moment().add(tMinutesTillTrain, "minutes");
+      var nextTrain = moment(nextTrainEq).format("hh:mm");
+
+
+      database.ref().push({
+          trainName: $("#train").val().trim(),
+          destination: $("#place").val().trim(),
+          frequency: $("#minutes").val().trim(),
+          nextTrain: nextTrain,
+          minutesAway: tMinutesTillTrain
+      })
+    $("#train").val("")
+    $("#place").val("")
+    $("#time").val("")
+    $("#minutes").val("");
+  });
+  
+  database.ref().on("child_added", function(snapshot){
+      console.log(snapshot.val());
+
+      var newRow = $("<tr>").append(
+          $("<td>").text(snapshot.val().destination),
+          $("<td>").text(snapshot.val().trainName),
+          $("<td>").text(snapshot.val().frequency),
+          $("<td>").text(snapshot.val().nextTrain),
+          $("<td>").text(snapshot.val().minutesAway),
+      )
+      $("#tbody").append(newRow)
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+  })
