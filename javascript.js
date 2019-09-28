67de5a48c54dee87c3a34b65a1d89f204f09d867
Emma Weber <emma.weber327@gmail.com>
2019-09-28 11:49:12 -0600

    Updated the time calculation so that the next train time and how long you have to wait updates on each refresh/time the page is visited.

--- javascript.js
+++ javascript.js
@@ -18,23 +18,14 @@ var firebaseConfig = {
   $("#add").on("click", function(){
       event.preventDefault();
 
-      firstTrain = $("#time").val()
-      frequency = $("#minutes").val()
-      var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years")
-      var currentTime = moment();
-      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-      var tRemainder = diffTime % frequency;
-      var tMinutesTillTrain = frequency - tRemainder;
-      var nextTrainEq = moment().add(tMinutesTillTrain, "minutes");
-      var nextTrain = moment(nextTrainEq).format("hh:mm A");
+     
 
 
       database.ref().push({
           trainName: $("#train").val().trim(),
           destination: $("#place").val().trim(),
           frequency: $("#minutes").val().trim(),
-          nextTrain: nextTrain,
-          minutesAway: tMinutesTillTrain
+          firstTrain: $("#time").val().trim()
       })
     $("#train").val("")
     $("#place").val("")
@@ -45,12 +36,22 @@ var firebaseConfig = {
   database.ref().on("child_added", function(snapshot){
       console.log(snapshot.val());
 
+      firstTrain = snapshot.val().firstTrain
+      frequency = snapshot.val().frequency
+      var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years")
+      var currentTime = moment();
+      var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+      var tRemainder = diffTime % frequency;
+      var tMinutesTillTrain = frequency - tRemainder;
+      var nextTrainEq = currentTime.add(tMinutesTillTrain, "minutes");
+      var nextTrain = moment(nextTrainEq).format("hh:mm A");
+
       var newRow = $("<tr>").append(
           $("<td>").text(snapshot.val().destination),
           $("<td>").text(snapshot.val().trainName),
           $("<td>").text(snapshot.val().frequency),
-          $("<td>").text(snapshot.val().nextTrain),
-          $("<td>").text(snapshot.val().minutesAway),
+          $("<td>").text(nextTrain),
+          $("<td>").text(tMinutesTillTrain),
       )
       $("#tbody").append(newRow)
     }, function(errorObject) {
